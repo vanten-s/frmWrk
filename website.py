b454f70fbb366ad8b6ec84c8a6d3ca47549264f2
vanten-s <vanten-s@vanten-s.com>
2023-08-05 10:24:57 +0200

    Forgor that xml was a type

--- website.py
+++ website.py
@@ -96,6 +96,7 @@ content_type = {
                 'html': 'text/html; charset=\'utf-8\'',
                 'css': 'text/css; charset=\'utf-8\'',
                 'js': 'application/javascript; charset=\'utf-8\'',
+                'xml': 'application/xml; charset=\'utf-8\'',
                 'png': 'image/png',
                 'jpg': 'image/jpeg',
                 'jpeg': 'image/jpeg',
